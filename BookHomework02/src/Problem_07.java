d41c4d4408396b0daa30b9ba674cab641750db79
Kaldirion <kaldirion@gmail.com>
2014-04-25 23:32:07 +0300

    basic java

---
+++ BookHomework02/src/Problem_07.java
@@ -0,0 +1,15 @@
+
+public class Problem_07 {
+
+	public static void main(String[] args) {
+		// calculate the weight on the Moon, given the weight on Earth
+		
+		float WeightEarth = 75.4f;
+		
+		float WeightMoon = 0.17f*WeightEarth;
+		
+		System.out.println(WeightMoon);
+
+	}
+
+}
---
+++ BookHomework02/src/Problem_08.java
@@ -0,0 +1,9 @@
+
+public class Problem_08 {
+
+	public static void main(String[] args) {
+		// check if given point is in the circle K [(0,0),5]
+
+	}
+
+}
